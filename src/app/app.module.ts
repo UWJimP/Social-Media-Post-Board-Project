d16717a25ff3f339a0e847e706e35b6ef041f490
Jim Phan <akari0@uw.edu>
2020-01-25 04:58:40 -0800

    Initial Set up
    Initial set up of the App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { PostBoardComponent } from './post-board/post-board.component';
 import { PostItemComponent } from './post-board/post-item/post-item.component';
 import { PostInputComponent } from './post-board/post-input/post-input.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostBoardComponent,
     PostItemComponent,
-    PostInputComponent
+    PostInputComponent,
+    SideMenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
