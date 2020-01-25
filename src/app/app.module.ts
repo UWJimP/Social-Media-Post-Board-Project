ac9a4e0581bc561e5dd5afde48824e88db1ced6c
Jim Phan <akari0@uw.edu>
2020-01-25 04:05:53 -0800

    Generate Post Board
    Generated the Post Board, Post Item, and Post Input components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostBoardComponent } from './post-board/post-board.component';
+import { PostItemComponent } from './post-board/post-item/post-item.component';
+import { PostInputComponent } from './post-board/post-input/post-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostBoardComponent,
+    PostItemComponent,
+    PostInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/post-board/post-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-board',
+  templateUrl: './post-board.component.html',
+  styleUrls: ['./post-board.component.css']
+})
+export class PostBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
