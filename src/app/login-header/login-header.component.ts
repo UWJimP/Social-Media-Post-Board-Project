f21142d4140bb06be78814842d302e440978fbaa
Jim Phan <akari0@uw.edu>
2020-01-31 00:20:59 -0800

    Update to Login-Header
    Updates to the Login-Header component.

--- src/app/login-header/login-header.component.ts
+++ src/app/login-header/login-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-header',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginHeaderComponent implements OnInit {
 
+  loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required])
+    })
   }
 
+  login() {
+    console.log(this.loginForm.get("email").value);
+    console.log(this.loginForm.get("password").value);
+  }
 }
