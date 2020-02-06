9fc72273cdea7caa4f166968c6dc6c6b322bee9c
Jim Phan <akari0@uw.edu>
2020-02-06 07:16:41 -0800

    Implemented Auth Component
    Implemented features of the Auth Component. The following features have been implemented so far:
    -Sign up Form created to accept a full name, email, and password.
    -Password requires special conditions to succeed.
    -Display error messages for what is incorrect with the sign up form.
    -Button implemented to allow resetting of the form.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  signupForm: FormGroup;
+  errorForm: {first: boolean, last: boolean, email: boolean, password: boolean};
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeForm();
   }
 
+  /**
+   * Initializes the form and the error platform ahead of time.
+   */
+  private initializeForm() {
+    this.signupForm = new FormGroup({
+      first_name: new FormControl(null, Validators.required),
+      last_name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required,
+      Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,16}$/)])
+    });
+    this.errorForm = {first: true, last: true, email: true, password: true};
+  }
+
+  /**
+   * When the form is submitted, checks for errors.
+   */
+  onSubmit() {
+    //console.log("Submitted");
+    if(this.signupForm.valid) {
+      //Navigate to the form to the main page upon logging in.
+    } else {
+      //Display an error message.
+      let controls = this.signupForm.controls;
+      this.errorForm = {first: controls.first_name.valid, last: controls.last_name.valid,
+      email: controls.email.valid, password: controls.password.valid};
+    }
+    //console.log(this.signupForm.controls.email.valid);
+  }
+
+  clickReset() {
+    //this.initializeForm();
+    this.signupForm.reset();
+    this.errorForm = {first: true, last: true, email: true, password: true};
+  }
 }
